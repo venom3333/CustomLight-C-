ff710ad55ad923aeb4326de496f611a67dce29d6
Vladimir Venom <venom3333@gmail.com>
2017-06-25 14:26:58 +0300

    First Commit

---
+++ CustomLight.Domain/Entities/Category.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace CustomLight.Domain.Entities
+{
+	public class Category
+	{
+		public int Id { get; set; }
+
+		[Required]
+		public string Name { get; set; }
+		public string Description { get; set; }
+		public string ShortDescription { get; set; }
+		public byte[]  Icon { get; set; }
+		public string IconMimeType { get; set; }
+	}
+}
---
+++ CustomLight.Domain/Entities/Image.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace CustomLight.Domain.Entities
+{
+	public class Image
+	{
+		public int Id;
+
+		public byte[] ImageData { get; set; }
+		public string ImageMimeType { get; set; }
+
+		public virtual ICollection<Project> Projects{ get; set; }
+		public virtual ICollection<Product> Products{ get; set; }
+	}
+}
