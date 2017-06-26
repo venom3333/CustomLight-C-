329c6b061c3a87059cf7e192ca845362810e2680
Vladimir Venom <venom3333@gmail.com>
2017-06-27 00:33:24 +0300

    В конструктор Category добавлено заполнение Created и Updated

--- CustomLight.WebUI/Controllers/HomeController.cs
+++ CustomLight.WebUI/Controllers/HomeController.cs
@@ -16,12 +16,12 @@ namespace CustomLight.WebUI.Controllers
 		{
 			using (Entities db = new Entities())
 			{
-				for (int i = 0; i < 8; i++)
-				{
-					Category cat = new Category { Name = "Prob_" + i, Description = "TEasdasdST!!_" + i, ShortDescription = "LALALffffA!!_" + i };
-					db.Categories.Add(cat);
-				}
-				db.SaveChanges();
+				//for (int i = 0; i < 8; i++)
+				//{
+				//	Category cat = new Category { Name = "Prob_" + i, Description = "TEasdasdST!!_" + i, ShortDescription = "LALALffffA!!_" + i };
+				//	db.Categories.Add(cat);
+				//}
+				//db.SaveChanges();
 				this.Categories = db.Categories.ToList();
 			}
 		}
