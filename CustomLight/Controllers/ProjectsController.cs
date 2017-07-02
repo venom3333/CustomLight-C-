7ce7c1b5eff089d559ff3f66f6baf7cdfd2fa5cd
Vladimir Venom <venom3333@gmail.com>
2017-07-02 14:51:50 +0300

    Projects включены в логику представления

---
+++ CustomLight/Controllers/ProjectsController.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using CustomLight.Models;
+using System.Web.UI;
+
+namespace CustomLight.Controllers
+{
+    public class ProjectsController : Controller
+    {
+        private CustomLightEntities db = new CustomLightEntities();
+
+        // GET: Projects
+        public async Task<ActionResult> Index()
+        {
+            return View(await db.Projects.ToListAsync());
+        }
+
+        // GET: Projects/Details/5
+        public async Task<ActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Project project = await db.Projects.FindAsync(id);
+            if (project == null)
+            {
+                return HttpNotFound();
+            }
+
+			ViewBag.Categories = await db.Categories.ToListAsync();
+			return View(project);
+        }
+
+        // GET: Projects/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Projects/Create
+        // Чтобы защититься от атак чрезмерной передачи данных, включите определенные свойства, для которых следует установить привязку. Дополнительные 
+        // сведения см. в статье https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "Id,Name,Description,ShortDescription,Icon,IconMimeType,IsPublished,Created,Updated")] Project project)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Projects.Add(project);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            return View(project);
+        }
+
+        // GET: Projects/Edit/5
+        public async Task<ActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Project project = await db.Projects.FindAsync(id);
+            if (project == null)
+            {
+                return HttpNotFound();
+            }
+            return View(project);
+        }
+
+        // POST: Projects/Edit/5
+        // Чтобы защититься от атак чрезмерной передачи данных, включите определенные свойства, для которых следует установить привязку. Дополнительные 
+        // сведения см. в статье https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "Id,Name,Description,ShortDescription,Icon,IconMimeType,IsPublished,Created,Updated")] Project project)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(project).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(project);
+        }
+
+        // GET: Projects/Delete/5
+        public async Task<ActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Project project = await db.Projects.FindAsync(id);
+            if (project == null)
+            {
+                return HttpNotFound();
+            }
+            return View(project);
+        }
+
+        // POST: Projects/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            Project project = await db.Projects.FindAsync(id);
+            db.Projects.Remove(project);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+		[OutputCache(Duration = 3600, Location = OutputCacheLocation.Client, VaryByParam = "id")]
+		public FileContentResult GetProjectIcon(int Id)
+		{
+			Project Project = db.Projects
+				.FirstOrDefault(p => p.Id == Id);
+
+			if (Project.Icon != null)
+			{
+				return File(Project.Icon, Project.IconMimeType);
+			}
+			else
+			{
+				return null;
+			}
+		}
+
+		[OutputCache(Duration = 3600, Location = OutputCacheLocation.Client, VaryByParam = "ImageId")]
+		public FileContentResult GetProjectImage(int ImageId)
+		{
+			ProjectImage image = db.ProjectImages.FirstOrDefault(i => i.Id == ImageId);
+
+			if (image != null)
+			{
+				return File(image.ImageData, image.ImageMimeType);
+			}
+			else
+			{
+				return null;
+			}
+		}
+	}
+}
